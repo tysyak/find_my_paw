2e386c0776d72bcc2145da17718a9d9721c3ebbd
Cristian Romero Andrade <mascrit@gmail.com>
2021-08-15 00:08:52 -0500

    ejemplo de mapa
    Signed-off-by: Cristian Romero Andrade <mascrit@gmail.com>

--- find_my_paw/lib/pages/find_map.dart
+++ find_my_paw/lib/pages/find_map.dart
@@ -4,23 +4,41 @@ import 'package:find_my_paw/assets/color.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class PagesFindMap extends StatelessWidget {
-  const PagesFindMap({Key? key}) : super(key: key);
 
-  // @override
+class PagesFindMap extends StatefulWidget {
+  @override
+  State<PagesFindMap> createState() => PagesFindMapState();
+}
+
+class PagesFindMapState extends State<PagesFindMap> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  late GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(19.3313833,-99.1868951);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Find Map')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'Blank...',
-            ),
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        drawer: NavBar(),
+        appBar: AppBar(
+          title: Text('Maps Sample App'),
+          backgroundColor: AssetsColor.getLayout(),
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
         ),
       ),
     );
   }
+
 }
