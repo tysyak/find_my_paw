4f17701030dbda90c13f8fc1e8902b92a3905295
Cristian Romero Andrade <mascrit@gmail.com>
2021-08-17 21:51:21 -0500

    mapa e ingreso de coordenadas

--- find_my_paw/lib/assets/color.dart
+++ find_my_paw/lib/assets/color.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-class AssetsColor{
-  static const Color layout = Color.fromARGB(255 ,248, 114, 118);
+
+class AssetsColor {
+  static const Color layout = Color.fromARGB(255, 248, 114, 118);
+  static const Color layout_t = Color.fromARGB(70, 248, 114, 118);
 
   static getLayout() => layout;
+  static getLayoutT() => layout_t;
 }
